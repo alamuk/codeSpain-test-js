b8eaeae36dcb2d25f709c33f73824bafbff0fa67
ALAM <alam.2008@live.co.uk>
2024-05-22 00:52:07 +0100

    async options for test

--- src/components/repositories/RepositoriesListItem.test.js
+++ src/components/repositories/RepositoriesListItem.test.js
@@ -17,8 +17,44 @@ function renderRepositoriesComponent() {
             <RepositoriesListItem repository={repository} />
         </MemoryRouter>
     );
+
+    return { repository };
 }
 
-test("shows the link in the github home page", () => {
-    renderRepositoriesComponent();
+test("shows the link in the github home page", async () => {
+    const { repository } = renderRepositoriesComponent();
+
+    await screen.findByRole("img", { name: "Javascript" });
+
+    const link = screen.getByRole("link", { name: /github repository/i });
+    expect(link).toHaveAttribute("href", repository.html_url);
 });
+
+// import { act } from "@testing-library/react";
+
+// if We don't want to render a element/component in from my components
+//
+// jest.mock("../tree/FileIcon", () => {
+//     return () => {
+//         return "File Icon Component";
+//     };
+// });
+//
+// inside test function:
+// screen.debug();
+// await pause();
+// screen.debug();
+
+// DON'T USE THIS FUNCTION FOR ANY CASE
+// await act(async () => {
+//     await pause();
+// })
+
+// outside the test function:
+// const pause = () => {
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             resolve();
+//         }, 100);
+//     });
+// };
